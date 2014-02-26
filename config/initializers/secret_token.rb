ab4a4705ca7d0379acc904d053e6ffd620a031a8
Muhammad Afzal <afzal.masood11@gmail.com>
2014-02-26 12:42:51 +0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AfzalPortfolio::Application.config.secret_key_base = 'b95af4badc45412f7b786c1725999a6352d3c8ef1ee1a01f2eaeb005adf2b8b3007cf867660532e01c0e88fde7781a010b1a235c71ceb8604af789083d9931fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AfzalPortfolio::Application.config.session_store :cookie_store, key: '_afzal-portfolio_session'
